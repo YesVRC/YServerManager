f000919c495c606c2469dcc7e192f75cfdf89a43
YesVRC <yes@ysnt.live>
2026-01-22 03:14:32 -0500

    PART 2

--- main.go
+++ main.go
@@ -1,1 +1,30 @@
 ﻿package main
+
+import (
+	"embed"
+	"net/http"
+
+	"github.com/YesVRC/YServerManager/components"
+	"github.com/YesVRC/YServerManager/middleware"
+	"github.com/a-h/templ"
+)
+
+//go:embed assets
+var assets embed.FS
+
+func main() {
+	s := http.NewServeMux()
+	s.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		templ.Handler(components.Hello(
+			components.PageMetadata{
+				Title: "Hello",
+				Path:  r.URL.Path,
+			},
+			"Yes")).ServeHTTP(w, r)
+	})
+	s.Handle("GET /assets/", http.FileServer(http.FS(assets)))
+
+	if err := http.ListenAndServe(":8080", middleware.LoggerMiddleware(s)); err != nil {
+		panic(err)
+	}
+}
